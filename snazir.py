9593623a3132915d6d72a249cb1329dd48944d67
Nazirjan93 <134106605+Nazirjan93@users.noreply.github.com>
2023-05-20 02:14:59 +0430

    Add files via upload

---
+++ snazir.py
@@ -0,0 +1,403 @@
+#Author SaidNazirDanesh
+
+
+
+pkg install git 
+
+
+'(pkg) update 
+
+
+('python3') git 
+
+('pip') install requests 
+
+
+('pip') mechanize 
+
+('pkg')('install')('git') python (2)
+
+
+
+import os, platform
+os.system('git pull')
+try:os.system('nazir ok')
+except:pass
+try:os.system('nazir cp')
+except:pass
+try:
+
+   import requests
+
+except:
+
+   os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from nazir64 import*
+    menu().main()
+elif bit == '32bit':
+    from nazir32 import*
+    menu().main()
+                          
+============================================================================================================================================================================================
+  
+('im lnistalgit  'form nazir')
+
+
+('install mechanize  'form nazir')
+
+
+                                                                	 ##          	  	      	    	 #####      ###########									
+"¥¥}lnistal git nazir¢  ##	 	   ###	           ## 	 ##	     ######## 	       ##	      ##			 ##									
+"¥¥}lnistal git nazir¢  ##		 ## ##	    	## 	  	 ##				  ##			##		  ##			##																																													
+"¥¥}lnistal git nazir¢  ##	 ##	 ##  	     ############	  		##			   ##	      #########																						   																					
+"¥¥}lnistal git nazir¢  ##   ##       ##           ##	          ## 		  ##				  ##		  ##		 	##																									
+"¥¥}lnistal git nazir¢  ####	      ##    	   ##	          ##		#########	   ####		##				##																		           																	
+
+=======================================================================================================================================================================================
+=======================================================================================================================================================================================
+
+Faceboof    :SaidNazirDanesh
+Yotiob       : S Nazir Danesh 
+Github      : https://github.com/Nazirjan960/NazirJan001
+Telegram   : @nazirjan960
+Watsapp    :            No 
+=========================================================================================================
+except:lnistal git ('pull')
+
+ ID [1] AFGHANSTAN
+
+(1)(main)M1
+(2)(main)M2
+(3)(main)M3
+
+ im from ('Roshan')
+ im from ('Etselat')
+ im form ('MTN')
+ im form ('Srlam')
+
+
+
+(1)Roshan 
+(2)Etselat
+(3)MTN
+(4)Selam
+
+
+Install:nazir.
+
+
+				[CODE MENU ]
+
+(1)079
+(2)072
+(3)078
+(4)070
+
+
+(Code 
+
+import os, platform
+os.system('git pull')
+try:os.system('nazir ok')
+except:pass
+try:os.system('nazir cp')
+except:pass
+try:
+
+   import requests
+
+except:
+
+   os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from nazir64 import*
+    menu().main()
+elif bit == '32bit':
+    from nazir32 import*
+    menu().main()
+('pip install git )('mtn')
+
+'Ryse Nazir '(OK)
+ Commands:RyseNazir
+
+('ok')$¥¢$¢®¥¥¥¥¥¥¥¥®©™RyseNazir@ 	 -_-
+('ok')$¥¢$¢®¥¥¥¥¥¥¥¥®©™RyseNazir@ 	 -_-
+('ok')$¥¢$¢®¥¥¥¥¥¥¥¥®©™RyseNazir@ 	 -_-
+('ok')$¥¢$¢®¥¥¥¥¥¥¥¥®©™RyseNazir@ 	 -_-
+('ok')$¥¢$¢®¥¥¥¥¥¥¥¥®©™RyseNazir@      -_-
+('ok")$¥¢$¢®¥¥¥¥¥¥¥¥®©™RyseNazir@	  -_-
+ 
+ 
+ Nazir ('CP')
+
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+
+
+
+
+
+
+
+(ID) Facebook Account %3A2
+%3A1679335279
+&#1776
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+import os, platform
+os.system('git pull')
+try:os.system('nazir ok')
+except:pass
+try:os.system('nazir cp')
+except:pass
+try:
+
+
+except:
+	
+	
+	
+   import requests
+
+
+   os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from nazir64 import*
+    menu().main()
+elif bit == '32bit':
+    from nazir32 import*
+    menu().main()
+
+
+
+Password list :۱۲۳۴۵۶:::۱۲۳۴۵۶۷۸۹::200300:400500:100200:123456:700800.900900.10002000.30004000
+
+
+
+from nazir import*
+    menu().main()
+elif bit == '32bit':
+	
+	
+(import updates
+import updates
+
+bit = nazir form platform.architecture()[1]
+if bit =='32 bit':
+form nazir32 import*
+menu().main()
+
+Pip install requests
+
+('#######
+Im Facebook Account me S Nazir Danesh
+ im port update ()main()
+ menu()nazir.pyhtoon.py
+
+
+
+import requests
+('main') if bit.==='64 bit':
+	form nazir64 import*
+	menu().main()
+	
+	#nazir.git clone
+
+	 
+	                                                              	 ##          	  	  	    	 #####         ###########									
+"¥¥}lnistal git nazir¢  ##	 	   ###	           ## 	 ##	     ######## 	       ##	      ##			 ##									
+"¥¥}lnistal git nazir¢  ##		 ## ##	    	## 	  	 ##				  ##			##		  ##			##																																													
+"¥¥}lnistal git nazir¢  ##	 ##	 ##  	     ############	  		##			   ##	      #########																						   																					
+"¥¥}lnistal git nazir¢  ##   ##       ##           ##	          ## 		  ##				  ##		  ##		 	##																									
+"¥¥}lnistal git nazir¢  ####	      ##    	   ##	          ##		#########	   ####		##				##																		           																	
+  
+  =======================================================================================================================================================================================
+
+Faceboof    :SaidNazirDanesh
+Yotiob       : S Nazir Danesh 
+Github      : https://github.com/Nazirjan960/NazirJan001
+Telegram   : @nazirjan960
+Watsapp    :            No 
+=========================================================================================================
+except:lnistal git ('pull')
+
+
+
+('Commands')
+('minu')nazir 
+
+  except:lnistal git ('pull')
+
+ ID [1] AFGHANSTAN
+
+(1)(main)M1
+(2)(main)M2
+(3)(main)M3
+
+ im from ('Roshan')
+ im from ('Etselat')
+ im form ('MTN')
+ im form ('Srlam')
+
+
+
+(1)Roshan 
+(2)Etselat
+(3)MTN
+(4)Selam
+
+
+Install:nazir.
+
+
+				[CODE MENU ]
+
+(1)079
+(2)072
+(3)078
+(4)070
+
+
+(Code 
+
+import os, platform
+os.system('git pull')
+try:os.system('nazir ok')
+except:pass
+try:os.system('nazir cp')
+except:pass
+try:
+
+   import requests
+
+except:
+
+   os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from nazir64 import*
+    menu().main()
+elif bit == '32bit':
+    from nazir32 import*
+    menu().main()
+('pip install git )('mtn')
+
+'Ryse Nazir '(OK)
+ Commands:RyseNazir
+
+('ok')$¥¢$¢®¥¥¥¥¥¥¥¥®©™RyseNazir@ 	 -_-
+('ok')$¥¢$¢®¥¥¥¥¥¥¥¥®©™RyseNazir@ 	 -_-
+('ok')$¥¢$¢®¥¥¥¥¥¥¥¥®©™RyseNazir@ 	 -_-
+('ok')$¥¢$¢®¥¥¥¥¥¥¥¥®©™RyseNazir@ 	 -_-
+('ok')$¥¢$¢®¥¥¥¥¥¥¥¥®©™RyseNazir@      -_-
+('ok")$¥¢$¢®¥¥¥¥¥¥¥¥®©™RyseNazir@	  -_-
+ 
+ 
+ Nazir ('CP')
+
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+('cp')$¥¢$¢®¥¥¥¥¥¥¥¥©®™NazirAfg@	'='
+
+
+
+
+
+
+
+(ID) Facebook Account %3A2
+%3A1679335279
+&#1776
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+import os, platform
+os.system('git pull')
+try:os.system('nazir ok')
+except:pass
+try:os.system('nazir cp')
+except:pass
+try:
+
+
+except:
+	
+	
+	
+   import requests
+
+
+   os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from nazir64 import*
+    menu().main()
+elif bit == '32bit':
+    from nazir32 import*
+    menu().main()
+
+
+
+Password list :۱۲۳۴۵۶:::۱۲۳۴۵۶۷۸۹::200300:400500:100200:123456:700800.900900.10002000.30004000
+
+
+
+from nazir import*
+    menu().main()
+elif bit == '32bit':
+	
+	
+(import updates
+import updates
+
+bit = nazir form platform.architecture()[1]
+if bit =='32 bit':
+form nazir32 import*
+menu().main()
+
+Pip install requests
+
+('#######
+Im Facebook Account me S Nazir Danesh
+ im port update ()main()
+ menu()nazir.pyhtoon.py
+
+
+
+import requests
+('main') if bit.==='64 bit':
+	form nazir64 import*
+	menu().main()
+	
+	#nazir.git clone
+
+('nazir')'(git pull')
+pkg install python2 
+
+
+pip install requests 
+
+
+	
